f20929506b33bd21d0335e9cf5d189c0112464c9
Nguyễn Thế Chí Dũng <dungntc108@gmail.com>
2020-06-06 08:43:03 +0700

    add code

--- src/frsl/gui/FRSLSpecificationTextEditor.java
+++ src/frsl/gui/FRSLSpecificationTextEditor.java
@@ -2,13 +2,17 @@ package frsl.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Insets;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+
+import org.fife.ui.autocomplete.AutoCompletion;
+import org.fife.ui.autocomplete.BasicCompletion;
+import org.fife.ui.autocomplete.CompletionProvider;
+import org.fife.ui.autocomplete.DefaultCompletionProvider;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
 import frsl.util.IOUtil;
 
@@ -16,17 +20,34 @@ public class FRSLSpecificationTextEditor extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
-	private JTextArea textArea;
+	private RSyntaxTextArea textArea;
 
 	public FRSLSpecificationTextEditor() {
 		setLayout(new BorderLayout());
 		setPreferredSize(new Dimension(1100, 500));
-		textArea = new JTextArea();
-		textArea.setTabSize(2);
-		textArea.setMargin(new Insets(10, 10, 10, 10));
-		textArea.setFont(new Font("Serif", Font.PLAIN, 15));
-		JScrollPane scrollPane = new JScrollPane(textArea);
-		add(scrollPane);
+		textArea = new RSyntaxTextArea();
+//		textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+		textArea.setCodeFoldingEnabled(true);
+		add(new RTextScrollPane(textArea));
+		CompletionProvider provider = createCompletionProvider();
+		AutoCompletion ac = new AutoCompletion(provider);
+		ac.install(textArea);
+		add(new RTextScrollPane(textArea));
+	}
+
+	private CompletionProvider createCompletionProvider() {
+		DefaultCompletionProvider provider = new DefaultCompletionProvider();
+		String[] listKeyWord = { "Use case: ", "Brief description: ", "Actors: ", "Precondition: ", "Postcondition: ",
+				"Trigger: ", "Special requirement: ", "Basic flow: ", "Alternative flows: ", "Flow ", "Step " };
+		for (String keyWord : listKeyWord) {
+			provider.addCompletion(new BasicCompletion(provider, keyWord));
+		}
+//		provider.addCompletion(
+//				new ShorthandCompletion(provider, "sysout", "System.out.println(", "System.out.println("));
+//		provider.addCompletion(
+//				new ShorthandCompletion(provider, "syserr", "System.err.println(", "System.err.println("));
+		return provider;
+
 	}
 
 	public void loadFromFile(String filePath) {
